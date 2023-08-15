32b7692f0f5567b1b4d8b44bdb598f561d5b969e
Alex Dawson <arjunsivakumar2@gmail.com>
2023-08-15 17:53:53 +0530

    tokenizer training complete

--- tokenizer.py
+++ tokenizer.py
@@ -1,10 +1,51 @@
+import json
+
+class Token:
+    def __init__(self, c, parents=None):
+        self.ch = c
+        self.parents = parents
+        if self.parents is None:
+            self.chs = ' '+ self.ch + ' '
+        else:
+            self.chs = ' ' + ' '.join(self.parents) + ' '
+        
+    def __add__(self, other):
+        assert isinstance(other, Token), f'cant merge token and {type(other)}'
+        return Token(self.ch + other.ch, (self.ch, other.ch))
+
+    def __repr__(self):
+        return self.ch
+
+    def __eq__(self, other):
+        return self.ch == other.ch
+
+    def __hash__(self):
+        return hash((self.ch, self.parents))
+
+    def count_in(self, corpus, ccounts):
+        count = 0
+        for word, wcount in zip(corpus, ccounts):
+            c = word.count(self.chs)
+            count += wcount * c
+        return count
+
+    def json_dict(self):
+        return {'ch': self.ch, 'parents': self.parents}
 
 class BPEncoder:
     
-    def get_corpus(self):
-        with open('langgpt/data/data.txt', 'r', encoding='utf-8') as file:
+    def __init__(self):
+        self.tokens = []
+        
+    def save_tokens(self, filepath):
+        with open(filepath, 'w', encoding='utf-8') as file:
+            json.dump([obj.json_dict() for obj in self.tokens], file)
+        
+    def get_corpus(self, filepath):
+        with open(filepath, 'r', encoding='utf-8') as file:
             raw_data = file.read()
         
+        #store a dict with word: count for each unique word in the dataset
         corpus = {}
         corp = [line.split() for line in raw_data.splitlines()]
         for line in corp:
@@ -14,74 +55,67 @@ class BPEncoder:
                 else:
                     corpus[word] += 1
                     
-        tokens = set(c for c in raw_data if c not in ['\n', ' '])
+        #tokens is a sorted list of Tokens of 1 char length (excluding spaces and special tokens)
+        tokens = set(c for c in raw_data if c not in ['\n', ' ', '/'])
         tokens = sorted(list(tokens))
+        self.tokens = [Token(t) for t in tokens]
         
-        return corpus, tokens
+        return corpus
             
-    def learn(self, iters):
-        corpus, tokens = self.get_corpus()
+    def learn(self, inp_file,tlen):
+        corpusdict = self.get_corpus(inp_file)
         
-        #convert corpus to lists to keep easy tracking
-        ckeys, ccounts = [], []
-        for k, v in corpus.items():
-            ckeys.append('^'.join(ch for ch in k))
+        #split the corpus into two lists for easier access.
+        #each word is split with spaces to make token searching simpler
+        corpus, ccounts = [], []
+        for k, v in corpusdict.items():
+            corpus.append(' ' + ' '.join(c for c in k) + ' ')
             ccounts.append(v)
         
-        #tokens is already a list - no need for change
-        tcounts = [0 for _ in tokens]
+        #list of tokens that need their counts (and counts of their children) to be updated
+        newtokens = [t for t in self.tokens]
         
-        #the ones that need to be updated
-        newtokens = [t for t in tokens]
+        counts = {t: t.count_in(corpus, ccounts) for t in self.tokens} #count of each token
+        used = {t: True for t in self.tokens}                          #is this token already in vocab?
         
-        for iter in range(iters):
-            #find all token pairs that we should check
-            pairs = [(nt, t) for nt in newtokens for t in tokens]
-            pairs += [(t, nt) for nt in newtokens for t in tokens]
-            
-            #create count dict for all pairs
-            counts = {}
-            parents = {}
-            for pair in pairs:
-                #print('p', pair)
-                p = pair[0] + pair[1]
-                if p not in counts:
-                    counts[p] = 0
-                    parents[p] = pair
-            
-            #check if each pair is in each word and add counts
-            for k, v in counts.items(): 
-                for word in corpus:
-                    if k in word:
-                        counts[k] += corpus[word]
-                    
-            #add new token
-            for k, v in counts.items():
-                if k in tokens:
-                    tcounts[tokens.index(k)] = v
-            
-            for k, v in counts.items():
-                if k not in tokens and v == max([v for k, v in counts.items() if k not in tokens]):
-                    print(f'{iter}: {parents[k][0]} + {parents[k][1]} = {k} --> {v}')
-                    tokens.append(k)
-                    tcounts.append(v)
-                    
-                    for i in range(len(ckeys)):
-                        ckeys[i].replace(f'{parents[k][0]}-{parents[k][0]}', k)
-                    
-                    #print('parents', parents[k], counts[k])
-                    newtokens = [k, parents[k][0], parents[k][1]]
-                    break
-                    
-        print(len(tokens), 'tokens.')
-        for k, v in zip(tokens, tcounts):
-            print(k, end=' ')
-        print('')
+        print('init:', len(self.tokens), 'chars found.')
         
-        return tokens
+        while len(self.tokens) < tlen-3: #-3 to include space for specials
+            #create next batch of tokens from newly created tokens
+            #if e + r -> er, we only want to update counts for tokens made up of e, r and er
+            #since the counts are added to the global count pool, they will be compared 
+            #againts previously calculated counts for all other tokens
+            pairs = [nt for nt in newtokens]
+            pairs = [nt + t for nt in newtokens for t in self.tokens]
+            pairs += [t + nt for nt in newtokens for t in self.tokens if (t+nt) not in pairs]
+            
+            #find the count for newly made tokens and add them to the count pool
+            for t in pairs:
+                tc = t.count_in(corpus, ccounts)
+                counts[t] = tc
+                used[t] = False
+            
+            #add the best token to the vocab
+            best, bcount = None, -1
+            for t, c in counts.items():
+                if not used[t] and c > bcount:
+                    best, bcount = t, c
+                    
+            self.tokens.append(best)
+            
+            #tokens that need to be updated next iteration
+            newtokens = [best] + [Token(p) for p in best.parents]
+            
+            print(len(self.tokens), best.parents, best, bcount)
+            
+            #update the words in the corpus to include the new token.
+            #' s i m p l e r _ ' -> merge(e, r) -> ' s i m p l er _ '
+            for i in range(len(corpus)):
+                corpus[i] = corpus[i].replace(best.chs, f' {best.ch} ')
+                
+        #add special tokens
+        self.tokens = [Token('*'), Token('^'), Token('/')] + self.tokens
         
 b = BPEncoder()
-t = b.learn(250)
-
-with open('data/tokens.txt', 'w', encoding='utf-8') as file:
-    file.write('\n'.join(t))
+b.learn('langgpt/data/data.txt', 100)
+b.save_tokens('langgpt/data/tokens.json')
